94f2af141ff18badea1ce7b9106ae74501fe6822
Yureka Adhikari <adhikariyureka@gmail.com>
2024-10-17 19:07:36 +0545

    SLAYY!!

--- src/index.js
+++ src/index.js
@@ -1,14 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+class Customer extends React.Component {
+  render() {
+    return <h2> I am from {this.props.city} </h2>;
+  }
+}
+class Details extends React.Component {
+  render() {
+    return (
+      <div>
+        <h2> Hello! </h2> <Customer city="Kathmandu" />{" "}
+      </div>
+    );
+  }
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <>
+    <Details />
+  </>
 );
 
 // If you want to start measuring performance in your app, pass a function
